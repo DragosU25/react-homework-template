8b98ca2843e6b6303b20e85716a7a17005f4b882
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-06-28 19:16:05 +0200

    lectia 1

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,23 @@
+import Menu from '../Menu';
+
+function Sidebar() {
+  const isMenuVisible = true;
+  const menuConfig = [
+    {
+      icon: 'U',
+      name: 'University',
+    },
+    {
+      icon: 'F',
+      name: 'Faculties',
+    },
+  ];
+
+  return (
+    <aside>
+      <Menu items={menuConfig} isVisible={isMenuVisible} />
+    </aside>
+  );
+}
+
+export default Sidebar;
